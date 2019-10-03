5fec8332935da2a0276f83d1282b750ebc850924
lucidqdreams <matthew.quickenden@gmail.com>
2019-10-03 14:26:03 -0700

    first

--- install-rras.ps1
+++ install-rras.ps1
@@ -2,20 +2,20 @@ param
     (
         [Parameter(Mandatory=$true)]
         [String] $TargetRRASIP,
-
-        [String] $TargetIPRange
-        
+        [Parameter(Mandatory=$true)]
+        [String] $TargetIPRange,
+        [Parameter(Mandatory=$true)]
+        [securestring] $SharedSecret
     )
+    Start-Transcript C:\PerfLogs\RRASinstall.log
 
 $TargetIPRange = $TargetIPRange + ':100'
-
 Install-WindowsFeature Routing -IncludeManagementTools
-
 Install-RemoteAccess -VpnType VpnS2S
 
-sleep 10
-
+Start-Sleep 10
 Add-VpnS2SInterface -Name "remote" $TargetRRASIP -Protocol IKEv2 -AuthenticationMethod PSKOnly -SharedSecret "ilovestack!" -IPv4Subnet $TargetIPRange 
-
 Set-VpnS2SInterface remote -InitiateConfigPayload $false
+
+Stop-Transcript
 
