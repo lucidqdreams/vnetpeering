907a522b71a158484e776e2e6d002f20f4ed1d1e
lucidqdreams <matthew.quickenden@gmail.com>
2019-10-03 16:32:30 -0700

    Update install-rras.ps1

--- install-rras.ps1
+++ install-rras.ps1
@@ -14,16 +14,15 @@ Install-WindowsFeature Routing -IncludeManagementTools
 Install-RemoteAccess -VpnType VpnS2S
 
 Start-Sleep 10
-Add-VpnS2SInterface -Name "remote" $TargetRRASIP -Protocol IKEv2 -AuthenticationMethod PSKOnly -SharedSecret "" -IPv4Subnet $TargetIPRange 
+Add-VpnS2SInterface -Name "remote" $TargetRRASIP -Protocol IKEv2 -AuthenticationMethod PSKOnly -SharedSecret "ilovestack!" -IPv4Subnet $TargetIPRange -persistent
 Set-VpnS2SInterface -Name "remote" -InitiateConfigPayload $false 
-Set-VpnS2SInterface -Name "remote" -persistent
 
 Start-Sleep 10
 
 $i=1
 do {
     get-VpnS2SInterface
-    start-sleep 5
+    start-sleep 6
     $i++
     Write-output $i
 } while ($i -lt 10)
