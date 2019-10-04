0a3daf11598d6b2a2ab9e62c1c0d3b1eec444e36
lucidqdreams <matthew.quickenden@gmail.com>
2019-10-04 16:07:45 -0700

    dsc

---
+++ DSC/InstallRRAS.ps1
@@ -0,0 +1,33 @@
+configuration InstallRRAS
+{
+   param
+    (
+
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Node localhost
+    {
+        WindowsFeature Routing
+        {
+            Ensure = 'Present'
+            Name = 'Routing'
+
+        }
+        WindowsFeature RemoteAccessPowerShell
+        {
+            Ensure = 'Present'
+            Name = 'RSAT-RemoteAccess-PowerShell'
+            DependsOn = '[WindowsFeature]Routing'
+        }
+        Service RemoteAccess
+        {
+            Name        = "RemoteAccess"
+            StartupType = "Automatic"
+            State       = "Running"
+            DependsOn = '[WindowsFeature]Routing'
+        }
+        
+
+    }
+}
